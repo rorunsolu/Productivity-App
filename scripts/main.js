4d425704177dcdf73f03c358cbd891f6be59f669
Ray Orunsolu <rorunsolu@gmail.com>
2024-11-25 14:30:24 +0000

    removed deleted imports for scss files

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,4 @@ import '/styles/style.scss';
 import '/styles/button.scss';
 import '/styles/note.scss';
 import '/styles/popup.scss';
-import '/styles/error.scss';
 import '/styles/popup-edit.scss'; 
-import '/styles/popup-view.scss';
