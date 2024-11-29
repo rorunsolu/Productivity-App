a230df04fa389ee0643c7737f707025a4382cb1a
Ray Orunsolu <rorunsolu@gmail.com>
2024-11-29 19:05:25 +0000

    issue resolved and comment added for context incase it happens again

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,5 @@ import '/styles/popup-edit.scss';
 import '/styles/popup-tags.scss';
 import '/styles/sidebar.scss';
 import '/styles/filter.scss';
+
+//! If Vercel throws an error regarding "rollup failed to resolve" then it's because i tried to push a commit for main.js where I've imported a new scss file but have yet to push the actually scss file so Vercel woudn't see it in my repo because it only exists on my local machine NOT the repo. So I have to push the scss file first then push the main.js file.
