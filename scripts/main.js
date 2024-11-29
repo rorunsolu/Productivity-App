801ae17d319ed8842287aa8a008d45a1601b8a8b
Ray Orunsolu <rorunsolu@gmail.com>
2024-11-29 18:58:32 +0000

    import added

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ import '/styles/style.scss';
 import '/styles/button.scss';
 import '/styles/note.scss';
 import '/styles/popup.scss';
-import '/styles/popup-edit.scss'; 
+import '/styles/popup-edit.scss';
 import '/styles/popup-tags.scss';
 import '/styles/sidebar.scss';
 import '/styles/filter.scss';
