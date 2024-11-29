c743ebd99c9befb19654952dc9519af30ee932a8
Ray Orunsolu <rorunsolu@gmail.com>
2024-11-29 18:51:31 +0000

    added new imports

--- scripts/main.js
+++ scripts/main.js
@@ -3,4 +3,6 @@ import '/styles/button.scss';
 import '/styles/note.scss';
 import '/styles/popup.scss';
 import '/styles/popup-edit.scss'; 
-import '/styles/popup-tags.scss'; 
+import '/styles/popup-tags.scss';
+import '/styles/sidebar.scss';
+import '/styles/filter.scss';
